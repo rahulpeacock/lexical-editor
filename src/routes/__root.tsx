4a38d3f578499356ff63799096fe21562c9eb396
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-12-17 16:58:39 +0530

    enhance: make react-scan to scan only in development mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,10 +3,14 @@ import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { scan } from 'react-scan';
 import { Fragment } from 'react/jsx-runtime';
 
-scan({
-  enabled: true,
-  log: true, // logs render info to console (default: false)
-});
+function scanReactApp() {
+  scan({
+    enabled: true,
+    log: true, // logs render info to console (default: false)
+  });
+}
+
+process.env.NODE_ENV === 'development' && scanReactApp();
 
 export const Route = createRootRoute({
   component: () => (
